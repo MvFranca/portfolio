d30cba30fddd5fb729515f5311882c41b6d6e688
Marcos Vinicius <viniciusfranca09@hotmail.com>
2023-07-24 11:27:31 -0300

    Responsividade do mobile adicionada

--- src/ui/components/habilidades/Habilidades.tsx
+++ src/ui/components/habilidades/Habilidades.tsx
@@ -1,60 +1,58 @@
 import { useEffect, useState } from "react";
 import styles from "../../styles/habilidades/Habilidades.module.css";
 
-
-
 const Habilidades = () => {
+  const [habilidades, setHabilidades] = useState([]);
+  const [conceito, setConceito] = useState(
+    "Clique nos ícones para saber o que cada um significa."
+  );
 
-  const [habilidades, setHabilidades] = useState([])
-  const [conceito, setConceito] = useState('Clique nos ícones ao lado para saber o que cada um significa.')
-
-  async function hab(){
-    const api = await fetch('https://portfolio-mvfranca.vercel.app/Habilidades.json')
+  async function hab() {
+    const api = await fetch(
+      "https://portfolio-mvfranca.vercel.app/Habilidades.json"
+    );
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const dados = await api.json()
+    const dados = await api.json();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    setHabilidades(dados)
+    setHabilidades(dados);
   }
 
   useEffect(() => {
-    void hab()
-  }, [])
-
+    void hab();
+  }, []);
 
   return (
     <div className={styles.container}>
-      <div className={styles.esquerda}>
-        <h2 className={styles.titulo}>Habilidades</h2>
-        <div className={styles.habilidades}>
+      
+        
 
-          {
-            habilidades.map(hab => {
-              const {src, conceito} = hab 
-              const Src: string = src
-              return(
-              <div className={styles.redespessoais} >
-                <img src={`${Src}`} alt="" 
-                 onClick={
-                  () => {
-                    setConceito(conceito)
-                  }
-                  }
-                />
-              </div>
-              )
-            })
-          }  
-
+        <div className={styles.conteudo}>
+          
+          <div className={styles.esquerda}>
+          <h2 className={styles.titulo}>Habilidades</h2>
+          <div className={styles.habilidades}>
+            {habilidades.map((hab) => {
+              const { src, conceito } = hab;
+              const Src: string = src;
+              return (
+                <div className={styles.redespessoais}  onClick={() => {
+                  setConceito(conceito);
+                }}>
+                  <img
+                    src={`${Src}`}
+                    alt="" 
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className={styles.direita}>
-        <p>
-          {conceito}
-        </p>
+        <div className={styles.direita}>
+          <p>{conceito}</p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Habilidades;
-
