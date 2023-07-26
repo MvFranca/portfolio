7fa030bff46c01ea7e822181858c9699c08605c8
Marcos Vinicius <viniciusfranca09@hotmail.com>
2023-07-26 12:35:55 -0300

    Modificação total no portfolio

--- src/ui/components/sobre/sobre.tsx
+++ src/ui/components/sobre/sobre.tsx
@@ -1,25 +1,25 @@
 import styles from "../../styles/sobre/Sobre.module.css";
 import EsquerdaSobre from "./EsquerdaSobre";
 import Projetos from "../projetos/Projetos";
-import {useState, useEffect} from 'react'
+//import { useEffect} from 'react'
 
 const Sobre = () => {
-    const [habilidades, setHabilidades] = useState([]);
+    //const [habilidades, setHabilidades] = useState([]);
   
 
-    async function hab() {
-        const api = await fetch(
+    //async function hab() {
+       // const api = await fetch(
           "https://portfolio-mvfranca.vercel.app/Habilidades.json"
-        );
+     //   );
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const dados = await api.json();
+       // const dados = await api.json();
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        setHabilidades(dados);
-      }
+      //  setHabilidades(dados);
+      //}
 
-    useEffect(() => {
-        void hab();
-      }, []);    
+   // useEffect(() => {
+  //      void hab();
+  //    }, []);    
     
   return (
     <div className={styles.container}>
